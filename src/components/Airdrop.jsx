971c6768a2a36cda3f6990bd043e1a50edbd5457
Subham <subhamsingh5638@gmail.com>
2024-09-24 23:43:25 +0530

    modified component

--- src/components/Airdrop.jsx
+++ src/components/Airdrop.jsx
@@ -7,18 +7,17 @@ export function Airdrop(){
     const [amount , setAmount] = useState(0);
 
     async function sendAirdropToUser(){
-        let sol = amount * 1000000000;
-        await connection.requestAirdrop(wallet.publicKey, sol);
+        let solana = amount * 1000000000;
+        await connection.requestAirdrop(wallet.publicKey, solana);
         alert(`Airdropped ${amount} sol`);
         
     }
 
     return <div>
-        
+
         <input type="text" placeholder="Amount" onChange={(e)=> setAmount(e.target.value)}/>
         <button onClick={sendAirdropToUser}>Send Airdrop</button>
-        
-        
+    
 
     </div>
 
